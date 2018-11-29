3891f6355197db61d2fdc66365f41c8b43bb3c67
Jarry Shaw <JarryShaw@users.noreply.github.com>
2018-11-29 09:45:00 +0800

    Fixed minor bugs in distro script

--- make.sh
+++ make.sh
@@ -37,7 +37,7 @@ twine upload dist/* -r pypi --skip-existing
 twine upload dist/* -r pypitest --skip-existing
 
 # get version string
-version=$( cat f2format/__main__.py | grep "^f2format" | sed "s/f2format \(.*\)/\1/" )
+version=$( cat f2format/__main__.py | grep "^__version__" | sed "s/__version__ = '\(.*\)'/\1/" )
 
 # upload to GitHub
 git pull
