ea4fc5488e0474b1d096ea78559855bb229a3161
Jarry Shaw <jarryshaw@icloud.com>
2019-07-10 08:28:19 +0800

    Revised closure functions

--- f2format.py
+++ f2format.py
@@ -73,6 +73,184 @@ tbtrim.set_trim_rule(predicate, strict=True, target=ConvertError)
 # Main convertion implementation
 
 
+def parse(string, source, error_recovery=False):
+    """Parse source string.
+
+    Args:
+     - string -- str, context to be converted
+     - source -- str, source of the context
+     - error_recovery -- bool, see `parso.Grammar.parse` (default: `False`)
+
+    Envs:
+     - F2FORMAT_VERSION -- convert against Python version (same as `--python` option in CLI)
+
+    Returns:
+     - parso.python.tree.Module -- parso AST
+
+    Raises:
+     - ConvertError -- when `parso.ParserSyntaxError` raised
+
+    """
+    try:
+        return parso.parse(string, error_recovery=error_recovery,
+                            version=os.getenv('F2FORMAT_VERSION', F2FORMAT_VERSION[-1]))
+    except parso.ParserSyntaxError as error:
+        message = '%s: <%s: %r> from %s' % (error.message, error.error_leaf.token_type,
+                                            error.error_leaf.value, source)
+        raise ConvertError(message).with_traceback(error.__traceback__) from None
+
+
+def extract(node):
+    """Extract f-string components.
+
+    Args:
+     - node -- parso.python.tree.PythonNode, parso AST for f-string
+
+    Returns:
+     - str -- extracted f-string string components
+     - list[str] -- extracted f-string expressions
+
+    """
+    # FStringStart
+    string = re.sub(r'[fF]', '', node.children[0].get_code())
+
+    expr_list = list()
+    for child in node.children[1:-1]:
+        if child.type != 'fstring_expr':
+            string += child.get_code()
+            continue
+
+        # <Operator: {>
+        string += '{'
+
+        expr_str = ''
+        spec_str = ''
+        for expr in child.children[1:-1]:
+            # conversion
+            if expr.type == 'fstring_conversion':
+                string += expr.get_code().strip()
+            # format specification
+            elif expr.type == 'fstring_format_spec':
+                for spec in expr.children:
+                    if spec.type != 'fstring_expr':
+                        string += spec.get_code().strip()
+                        continue
+
+                    # <Operator: {>
+                    string += '{'
+
+                    for spec_expr in spec.children[1:-1]:
+                        if spec_expr.type == 'fstring_conversion':  # pragma: no cover
+                            string += spec_expr.get_code().strip()
+                        elif spec_expr.type == 'fstring_format_spec':  # pragma: no cover
+                            string += spec_expr.get_code().strip()
+                        elif spec_expr.type == 'testlist':  # pragma: no cover
+                            spec_str += '(%s)' % spec_expr.get_code()
+                        else:
+                            spec_str += spec_expr.get_code()
+
+                    # <Operator: }>
+                    string += '}'
+            # implicit tuple
+            elif expr.type == 'testlist':
+                expr_str += '(%s)' % expr.get_code()
+            # embedded f-string
+            elif expr.type == 'fstring':
+                text, expr = extract(expr)
+                expr_str += text
+                if expr:
+                    expr_str += '.format(%s)' % ', '.join(expr)
+            # concatenable strings
+            elif expr.type == 'strings':
+                text_temp_list = list()
+                expr_temp_list = list()
+                for expr_child in expr.children:
+                    if expr_child.type == 'fstring':
+                        text_temp, expr_temp = extract(expr_child)
+                        text_temp_list.append((True, text_temp))
+                        expr_temp_list.extend(expr_temp)
+                    else:
+                        text_temp_list.append((False, expr_child.get_code()))
+                if expr_temp_list:
+                    expr_str += ''.join(map(lambda text: text[1] if text[0]
+                                            else re.sub(r'([{}])', r'\1\1', text[1]), text_temp_list))
+                    expr_str += '.format(%s)' % ', '.join(expr_temp_list)
+                else:
+                    expr_str += ''.join(map(lambda text: text[1], text_temp_list))
+            # regular expression / debug f-string
+            elif expr.type == 'operator' and expr.value == '=':
+                next_sibling = expr.get_next_sibling()
+                if (next_sibling.type == 'operator' and next_sibling.value == '}') \
+                    or next_sibling.type in ['fstring_conversion', 'fstring_format_spec']:
+                    expr_tmp = expr_str + expr.get_code() + re.sub(r'\S+.*$', r'', next_sibling.get_code()) + '{}'
+                    expr_str = '%r.format(%s)' % (expr_tmp, expr_str)
+                else:  # pragma: no cover
+                    expr_str += expr.get_code()
+            # regular expression
+            else:
+                expr_str += expr.get_code()
+
+        if expr_str:  # pragma: no cover
+            expr_list.append(expr_str)
+        if spec_str:
+            expr_list.append(spec_str)
+
+        # <Operator: }>
+        string += '}'
+
+    # FStringEnd
+    string += node.children[-1].get_code()
+
+    return string, expr_list
+
+
+def walk(node):
+    """Walk parso AST.
+
+    Args:
+     - node -- parso.python.tree.Module, parso AST
+
+    Returns:
+     - str -- converted string
+
+    """
+    string = ''
+
+    if node.type == 'strings':
+        text_list = list()
+        expr_list = list()
+        for child in node.children:
+            if child.type == 'fstring':
+                text, expr = extract(child)
+                text_list.append((True, text))
+                expr_list.extend(expr)
+            else:
+                text_list.append((False, child.get_code()))
+        if expr_list:
+            string += ''.join(map(lambda text: text[1] if text[0]
+                                    else re.sub(r'([{}])', r'\1\1', text[1]), text_list))
+            string += '.format(%s)' % ', '.join(expr_list)
+        else:
+            string += ''.join(map(lambda text: text[1], text_list))
+        return string
+
+    if node.type == 'fstring':
+        text, expr = extract(node)
+        string += text
+        if expr:
+            string += '.format(%s)' % ', '.join(expr)
+        return string
+
+    if isinstance(node, parso.python.tree.PythonLeaf):
+        string += node.get_code()
+
+    if hasattr(node, 'children'):
+        for child in node.children:
+            string += walk(child)
+
+    return string
+
+
 def convert(string, source='<unknown>'):
     """The main conversion process.
 
@@ -87,147 +265,8 @@ def convert(string, source='<unknown>'):
      - str -- converted string
 
     """
-    def parse(string, error_recovery=False):
-        try:
-            return parso.parse(string, error_recovery=error_recovery,
-                               version=os.getenv('F2FORMAT_VERSION', F2FORMAT_VERSION[-1]))
-        except parso.ParserSyntaxError as error:
-            message = '%s: <%s: %r> from %s' % (error.message, error.error_leaf.token_type,
-                                                error.error_leaf.value, source)
-            raise ConvertError(message).with_traceback(error.__traceback__)
-
-    def extract(node):
-        # FStringStart
-        string = re.sub(r'[fF]', '', node.children[0].get_code())
-
-        expr_list = list()
-        for child in node.children[1:-1]:
-            if child.type != 'fstring_expr':
-                string += child.get_code()
-                continue
-
-            # <Operator: {>
-            string += '{'
-
-            expr_str = ''
-            spec_str = ''
-            for expr in child.children[1:-1]:
-                # conversion
-                if expr.type == 'fstring_conversion':
-                    string += expr.get_code().strip()
-                # format specification
-                elif expr.type == 'fstring_format_spec':
-                    for spec in expr.children:
-                        if spec.type != 'fstring_expr':
-                            string += spec.get_code().strip()
-                            continue
-
-                        # <Operator: {>
-                        string += '{'
-
-                        for spec_expr in spec.children[1:-1]:
-                            if spec_expr.type == 'fstring_conversion':  # pragma: no cover
-                                string += spec_expr.get_code().strip()
-                            elif spec_expr.type == 'fstring_format_spec':  # pragma: no cover
-                                string += spec_expr.get_code().strip()
-                            elif spec_expr.type == 'testlist':  # pragma: no cover
-                                spec_str += '(%s)' % spec_expr.get_code()
-                            else:
-                                spec_str += spec_expr.get_code()
-
-                        # <Operator: }>
-                        string += '}'
-                # implicit tuple
-                elif expr.type == 'testlist':
-                    expr_str += '(%s)' % expr.get_code()
-                # embedded f-string
-                elif expr.type == 'fstring':
-                    text, expr = extract(expr)
-                    expr_str += text
-                    if expr:
-                        expr_str += '.format(%s)' % ', '.join(expr)
-                # concatenable strings
-                elif expr.type == 'strings':
-                    text_temp_list = list()
-                    expr_temp_list = list()
-                    for expr_child in expr.children:
-                        if expr_child.type == 'fstring':
-                            text_temp, expr_temp = extract(expr_child)
-                            text_temp_list.append((True, text_temp))
-                            expr_temp_list.extend(expr_temp)
-                        else:
-                            text_temp_list.append((False, expr_child.get_code()))
-                    if expr_temp_list:
-                        expr_str += ''.join(map(lambda text: text[1] if text[0]
-                                                else re.sub(r'([{}])', r'\1\1', text[1]), text_temp_list))
-                        expr_str += '.format(%s)' % ', '.join(expr_temp_list)
-                    else:
-                        expr_str += ''.join(map(lambda text: text[1], text_temp_list))
-                # regular expression / debug f-string
-                elif expr.type == 'operator' and expr.value == '=':
-                    next_sibling = expr.get_next_sibling()
-                    if (next_sibling.type == 'operator' and next_sibling.value == '}') \
-                        or next_sibling.type in ['fstring_conversion', 'fstring_format_spec']:
-                        expr_tmp = expr_str + expr.get_code() + re.sub(r'\S+.*$', r'', next_sibling.get_code()) + '{}'
-                        expr_str = '%r.format(%s)' % (expr_tmp, expr_str)
-                    else:  # pragma: no cover
-                        expr_str += expr.get_code()
-                # regular expression
-                else:
-                    expr_str += expr.get_code()
-
-            if expr_str:  # pragma: no cover
-                expr_list.append(expr_str)
-            if spec_str:
-                expr_list.append(spec_str)
-
-            # <Operator: }>
-            string += '}'
-
-        # FStringEnd
-        string += node.children[-1].get_code()
-
-        return string, expr_list
-
-    def walk(node):
-        string = ''
-
-        if node.type == 'strings':
-            text_list = list()
-            expr_list = list()
-            for child in node.children:
-                if child.type == 'fstring':
-                    text, expr = extract(child)
-                    text_list.append((True, text))
-                    expr_list.extend(expr)
-                else:
-                    text_list.append((False, child.get_code()))
-            if expr_list:
-                string += ''.join(map(lambda text: text[1] if text[0]
-                                      else re.sub(r'([{}])', r'\1\1', text[1]), text_list))
-                string += '.format(%s)' % ', '.join(expr_list)
-            else:
-                string += ''.join(map(lambda text: text[1], text_list))
-            return string
-
-        if node.type == 'fstring':
-            text, expr = extract(node)
-            string += text
-            if expr:
-                string += '.format(%s)' % ', '.join(expr)
-            return string
-
-        if isinstance(node, parso.python.tree.PythonLeaf):
-            string += node.get_code()
-
-        if hasattr(node, 'children'):
-            for child in node.children:
-                string += walk(child)
-
-        return string
-
     # parse source string
-    module = parse(string)
+    module = parse(string, source)
 
     # convert source string
     string = walk(module)
@@ -278,6 +317,12 @@ __f2format_encoding__ = os.getenv('F2FORMAT_ENCODING', LOCALE_ENCODING)
 
 
 def get_parser():
+    """Generate CLI parser.
+
+    Returns:
+     - argparse.ArgumentParser -- CLI parser for f2format
+
+    """
     parser = argparse.ArgumentParser(prog='f2format',
                                      usage='f2format [options] <python source files and folders...>',
                                      description='Convert f-string to str.format for Python 3 compatibility.')
@@ -307,7 +352,12 @@ def get_parser():
 
 
 def main(argv=None):
-    """Entry point for f2format."""
+    """Entry point for f2format.
+
+    Args:
+     - argv -- list[str], CLI arguments (default: None)
+
+    """
     parser = get_parser()
     args = parser.parse_args(argv)
 
