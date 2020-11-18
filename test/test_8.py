f68f189a87d30224d758d507a0d30b8db52387af
gousaiyang <gousaiyang@163.com>
2020-11-18 11:16:14 -0800

    More test cases

--- test/test_8.py
+++ test/test_8.py
@@ -1,5 +1,8 @@
 print(f'1')
 print(f'escape{{brackets}}')
+print(f'start{{}}end')
+print(f'new start{ {} }new end')
+print(f'more start{ {1} }more end')
 print(f'{"u"!r}')
 print(f'{"v"!s}')
 print(f'{"w"!a}')
@@ -8,6 +11,7 @@ print(f'{"y":10}2')
 print(f'3{chr(20320)+chr(22909)!a:}4')
 print(f'5{"z"!s:10}6')
 print(f'{666,777=}')  # TODO: more unpacking for Python 3.5
+# print(f'{*[1],*[2]}')
 print(f'{"c"+"d" = }')
 print(f'{"e"=!r}')
 print(f'{"f"=!s}')
@@ -21,11 +25,23 @@ print(RF'''prefix\n
 suffix''')
 # TODO: more complicated nested replacement field
 
+print(f"{1if'yes'else'no'}")
+print(f'{x for x in [1]}'[:30])
+print(f'{(x for x in [1])}'[:30])
+print(f'{[x for x in [1]]}')
 
-def foo():
+
+def foo(x=0):
+    if x == 0:
+        yield 'end'
+        return
     print(f'hello {yield 88} world')
+    print(f'yet {(yield "99")} another')
+    print(f'{yield from foo()}')
 
 
-print(list(foo()))
+print(list(foo(1)))
 
+# 3.5+: async def foo(): f'{await 1}'
 # TODO: need more review of grammar, also need invalid cases
+# may need to split this into multiple files
