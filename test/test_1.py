b22627a16ecb515e6851583462548fedfe03de58
gousaiyang <gousaiyang@163.com>
2021-04-02 00:34:18 -0700

    Don't print non-ascii

--- test/test_1.py
+++ test/test_1.py
@@ -6,13 +6,13 @@ s1 = (f'vv:\377vv{v1}ww!{{}}ww{v2!r}\\xxx\x03{v2+"!"!s}{v2+"""}"""!a}{{llooo}}{{
       r'''yaaa\n'aa'''
       "wawa{nono}wa{{}}wa\\"
       RF"""xi'''{v2+'''w'''}xi{v2+'!'}k\k"""'kuux' + 'padding!')
-print(s1)
+print(ascii(s1))
 
 s2 = "f'fake{f}string!'"
 print(s2)
 
 s3 = f'yet_another_{s1,v2!r:<5}_fstring!:'.replace('another', 'other')
-print(s3)
+print(ascii(s3))
 
 s4 = f'{v2}'
 print(s4)
